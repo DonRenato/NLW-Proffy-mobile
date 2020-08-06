af1d7e7d509a6668c0813f6ab4fd04c79e8b4c5b
Renato  Avellar <onizuka@Mac-mini.local>
2020-08-06 17:26:37 -0300

    create LandingPage and GiveClassesPage

---
+++ src/pages/Study/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function Study(){
+   return(
+       <View>
+           <Text>
+                Study
+           </Text>
+       </View>
+   );
+}
+
+export default Study;
